c29f009ccb1f8df652f18eb04179c7ddea676663
“simon-nstein” <spstein@willamette.edu>
2023-08-26 19:55:28 -0700

    new

--- src/components/start.js
+++ src/components/start.js
@@ -1,6 +1,4 @@
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import SafeArea from 'react-safe-area-component'
 import "../components/start.css";
 
 export const Start = () => {
@@ -11,12 +9,10 @@ export const Start = () => {
     };
 
     return (
-        <SafeArea top bottom>
-            <div class="mainDiv">
-                <h1 class="heading">PollThru</h1>
-                <p class="descript">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                <button onClick={start} class="btn"> Start!</button>
-            </div>
-        </SafeArea>
+        <div class="mainDiv">
+            <h1 class="heading">PollThru</h1>
+            <p class="descript">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <button onClick={start} class="btn"> Start!</button>
+        </div>
     );
 };
