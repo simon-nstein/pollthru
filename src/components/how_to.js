3f298a0d75e5dec4711e4de7f4d359cc9ef3714d
“simon-nstein” <spstein@willamette.edu>
2023-08-27 21:49:50 -0700

    how to updates

--- src/components/how_to.js
+++ src/components/how_to.js
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
 import { GrClose } from 'react-icons/gr';
+import myGif from '../components/sliderGIF.gif';
 
 
 const HowTo = ({ isOpen, onClose }) => {
@@ -17,21 +18,27 @@ const HowTo = ({ isOpen, onClose }) => {
         const modalContentStyle = {
           width: "35%",
           margin: "0 auto",
+          fontFamily: "Poppins"
         };
+
+        const gifStyle = {
+          width: "40%",
+          display: "block",
+          margin: "0 auto"
+        }
         
         // Add media query for screens with width below 400px
         if (window.innerWidth < 500) {
           modalContentStyle.width = "82%";
           modalContentStyle.inset = "28px 14px";
         }
-        
+
 return (
     <Modal isOpen={isOpen} onRequestClose={onClose} style={{ content: modalContentStyle }}>
       <button style={closeButtonStyle} onClick={onClose}> <GrClose /> </button>
       <h1>How to Play</h1>
-      <p>1. Guess what percent of people chose an answer on yesterday's poll.</p>
-      <p><b>Example</b></p>
-      <p>Based on yesterday's poll, what percent of people choose Drake as the better artist than Jay-Z?</p>
+      <p>1. Use the slider to guess what percent of people chose an answer on yesterday's poll.</p>
+      <img src={myGif} alt="my-gif" style={ gifStyle }/>
       <p>2. Provide your input in the daily poll, which will be part of tomorrow's daily challenge.</p>
       <p>3. Come back tomorrow! A new challenge is released daily at midnight.</p>
 
